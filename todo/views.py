dbf62976853aa7a7ba39f9cbda25f46df95ca448
unknown <Shashank.huilgol22@gmail.com>
2024-12-28 12:35:24 +0530

    initial commit

---
+++ todo/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    return render(request, 'task_list.html', {'tasks': tasks, 'form': form})
+
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    return render(request, 'task_update.html', {'form': form})
+
+def task_delete(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'task_delete.html',{'task':task})
